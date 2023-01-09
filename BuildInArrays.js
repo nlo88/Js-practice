9d8f34641cca04c3a6e0188d5819c082e1e563b4
nlo88 <nloyeltylo@gmail.com>
2023-01-09 14:16:31 -0500

    array

--- BuildInArrays.js
+++ BuildInArrays.js
@@ -25,16 +25,39 @@ const users = [
 
 //console.log(users);
 
-for (let user of users){
-    console.log (user)
-}
+// for (let user of users){
+//     console.log (user)
+// }
 
-const user = users[0]
-for(let userProp in user) {
-    console.log(userProp) // name // age // give you properties
-    console.log(userProp, user[userProp]) // name Karen // age: 35
-}
+// const user = users[0]
+// for(let userProp in user) {
+//     console.log(userProp) // name // age // give you properties
+//     console.log(userProp, user[userProp]) // name Karen // age: 35
+// }
+
+// const filteredUsers = users.filter((user) => {
+//     return user.age < 30;
+// });
+
+// console.log(filteredUsers)
+
+// const findUser = users.find(user => {
+//     return user.name === 'James'
+// })
+// console.log(findUser)
+
+// const findIndex = users.findIndex(user => {
+//     return user.name === 'Jin';
+// })
+// console.log(findIndex)
 
 
+// const names = users.map(user => {
+//     return {
+//     ...user,
+//     description: `${user.name} is ${user.age} years old`
+//     }
+// })
 
-
+// console.log(users)
+// console.log(names)
