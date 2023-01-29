4c1821abfa39b9c520962620c5e728126512b745
nlo88 <nloyeltylo@gmail.com>
2023-01-29 13:14:01 -0500

    threevow

---
+++ hasThreeVowels.js
@@ -0,0 +1,11 @@
+function hasThreeVowels(str){
+    let vowel = 'aieou'
+    let vowelCount = 0;
+    for (let i=0; i<str.length; i++){
+        let char = str[i]
+        if (char === vowel){
+            vowelCount++
+        }
+    }
+    return vowelCount > 3
+}
