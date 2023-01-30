bd280c0cab2b2b8bfa8fef176ba7cc22fc24a160
nlo88 <nloyeltylo@gmail.com>
2023-01-30 12:02:12 -0500

    ceasarCipher

---
+++ caesarCipher.js
@@ -0,0 +1,15 @@
+function caesarCipher(string,num){
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let newString ='';
+
+    for(let i=0; i< string.length; i++){
+        let char = string[i]
+        let oldIndex = alphabet.indexOld(char)
+        let newIndex = oldIndex + num
+        let newChar = alphabet[newIndex % alphabet.length]
+        newString += newChar
+    }
+
+    return newString
+}
+    
