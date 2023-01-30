c21f98d2b03091195c470fecffdedc45536ee0a0
nlo88 <nloyeltylo@gmail.com>
2023-01-30 12:11:56 -0500

    updates

--- caesarCipher.js
+++ caesarCipher.js
@@ -4,7 +4,7 @@ function caesarCipher(string,num){
 
     for(let i=0; i< string.length; i++){
         let char = string[i]
-        let oldIndex = alphabet.indexOld(char)
+        let oldIndex = alphabet.indexOf(char)
         let newIndex = oldIndex + num
         let newChar = alphabet[newIndex % alphabet.length]
         newString += newChar
